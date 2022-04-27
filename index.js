b28e2a44d7f5ac0846febada8010996c12196f1a
harutas <96802323+harutas@users.noreply.github.com>
2022-04-28 00:26:15 +0900

    remove execution function
    実行関数部分を削除しました。

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-// ------------実行関数---------------
-function resultOnButton(){
-    const luck = getLuck();
-    const luckyLang = getLuckyLang();
-    const luckyDB = getLuckyDB();
-    const luckyEditor = getLuckyEditor();
-    const home = document.getElementById("home");
-    const result = document.getElementById("result");
-    const resultLuck = document.getElementById("result-luck");
-    const resultLang = document.getElementById("result-lang");
-    const resultDB = document.getElementById("result-DB");
-    const resultEditor = document.getElementById("result-editor");
-    resultLuck.innerHTML = luck;
-    resultLang.innerHTML = luckyLang;
-    resultDB.innerHTML = luckyDB;
-    resultEditor.innerHTML = luckyEditor;
-    home.classList.add("d-none");
-    result.classList.remove("d-none");
-    console.log(result);
-    console.log("on!!");
-}
-
-function resultOffButton(){
-    const home = document.getElementById("home");
-    const result = document.getElementById("result");
-    home.classList.remove("d-none");
-    result.classList.add("d-none");
-    console.log("off!!");
-}
-
 // ------------処理関数---------------
 // 運勢
 function getLuck(){
