a15b4689ff107f6cd421f877f79d72ccc0ecc22d
shindo-develop <shindo.develop@gmail.com>
2022-04-29 23:35:51 +0900

    Edit index.js fixes #24
    履歴機能と履歴詳細機能を作成。

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import { Omikuji } from '/omikuji.js';
 const config = {
     startBtn : document.getElementById("start-btn"),
     topPage : document.getElementById("top-page"),
-    resultPage : document.getElementById("result-page")
+    resultPage : document.getElementById("result-page"),
+    logPage : document.getElementById("log-page")
 }
 
 function displayBlock(target){
@@ -40,7 +41,7 @@ function createResultPage(omikujiObj){
         </div>
         <div class="w-90 mx-2 border-red border-gray">
             <p>✿ラッキー言語✿</p>
-            <p>${omikujiObj.luckyLang}}</p>
+            <p>${omikujiObj.luckyLang}</p>
         </div>
         <div class="w-90 mx-2 border-red border-gray">
             <p>✿ラッキーデータベース✿</p>
@@ -56,12 +57,55 @@ function createResultPage(omikujiObj){
         <div class="my-3">
             <button id="log-btn" type="button" class="btn btn-danger rounded-pill">過去の運勢を確認する</button>
         </div>
-        
     `
     container.querySelectorAll("#back-btn")[0].addEventListener("click", function(){
         displayNone(config.resultPage);
         displayBlock(config.topPage);
     });
-    
+
+    container.querySelectorAll("#log-btn")[0].addEventListener("click", function(){
+        displayNone(config.resultPage);
+        displayBlock(config.logPage);
+        createLogPage(Omikuji.omikujiLog);
+    });
+
     config.resultPage.append(container);
+}
+
+function createLogPage(omikujiLog){
+    config.logPage.innerHTML = ``;
+    let container = document.createElement("div");
+    container.classList.add("frame", "bg-white", "d-flex", "justify-content-center", "align-items-center", "flex-column", "mx-auto", "text-center", "text-danger");
+    container.innerHTML = `
+        <div class="w-90 mx-2 border-red">
+            <h1>ソフトウェア開発みくじ</h1>
+            <h3>過去${omikujiLog.length}回分の履歴</h3>
+        </div>
+        `
+    for(let i = 0; i < omikujiLog.length; i++){
+        container.innerHTML += `
+        <div class="w-90 mx-2 border-red border-gray">
+            <p class="font1q5rem">${i + 1}回目</p>
+            <p class="omikuji-log">${omikujiLog[i]["luck"]}</p>
+        </div>
+        `
+    }
+    container.innerHTML += `
+        <div class="my-3">
+            <button id="top-btn" type="button" class="btn btn-danger rounded-pill">トップページに戻る</button>
+        </div>
+    `
+    container.querySelectorAll("#top-btn")[0].addEventListener("click", function(){
+        displayNone(config.logPage);
+        displayBlock(config.topPage);
+    });
+
+    for(let i = 0; i < container.querySelectorAll(".omikuji-log").length; i++){
+        container.querySelectorAll(".omikuji-log")[i].addEventListener("click", function(){
+            displayNone(config.logPage);
+            displayBlock(config.resultPage);
+            createResultPage(omikujiLog[i]);
+        });
+    }
+    config.logPage.append(container);
 }
