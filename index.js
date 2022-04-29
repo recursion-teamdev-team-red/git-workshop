3e055d69703e18f1f2d71d1ad642b051582b4360
harutas <96802323+harutas@users.noreply.github.com>
2022-04-30 00:32:28 +0900

    fix styles

--- index.js
+++ index.js
@@ -30,32 +30,32 @@ function createResultPage(omikujiObj){
     container.classList.add("frame", "bg-white", "d-flex", "justify-content-center", "align-items-center", "flex-column", "mx-auto", "text-center", "text-danger");
     container.innerHTML = `
         <div class="w-90 mx-2 border-red">
-            <h1>ソフトウェア開発みくじ</h1>
+            <h1 class="my-3 font1rem font-bold">ソフトウェア開発みくじ</h1>
         </div>
         <div class="w-90 mx-2 border-red border-gray">
-            <img src="/image/${omikujiObj.resultImage}" alt="" width="200px">
-            <h2 class="text-danger font-bold font3rem">${omikujiObj.luck}</h2>
+            <img src="/image/${omikujiObj.resultImage}" class="my-3" alt="" width="200px">
+            <h2 class="text-danger font-bold font3rem my-3">${omikujiObj.luck}</h2>
         </div>
         <div class="w-90 mx-2 border-red border-gray">
-            <p>${omikujiObj.resultText}</p>
+            <p class="my-3">${omikujiObj.resultText}</p>
         </div>
         <div class="w-90 mx-2 border-red border-gray">
-            <p>✿ラッキー言語✿</p>
-            <p>${omikujiObj.luckyLang}</p>
+            <p class="my-3">✿ラッキー言語✿</p>
+            <p class="my-3">${omikujiObj.luckyLang}</p>
         </div>
         <div class="w-90 mx-2 border-red border-gray">
-            <p>✿ラッキーデータベース✿</p>
+            <p class="my-3">✿ラッキーデータベース✿</p>
             <p>${omikujiObj.luckyDB}</p>
         </div>
         <div class="w-90 mx-2 border-gray">
-            <p>✿ラッキーエディタ✿</p>
-            <p>${omikujiObj.luckyEditor}</p>
+            <p class="my-3">✿ラッキーエディタ✿</p>
+            <p class="my-3">${omikujiObj.luckyEditor}</p>
         </div>
-        <div class="my-3">
-            <button id="back-btn" type="button" class="btn btn-danger rounded-pill">もう一度おみくじを引く</button>
+        <div class="my-2 w-100">
+            <button id="back-btn" type="button" class="btn btn-danger rounded-pill w-90">もう一度おみくじを引く</button>
         </div>
-        <div class="my-3">
-            <button id="log-btn" type="button" class="btn btn-danger rounded-pill">過去の運勢を確認する</button>
+        <div class="mb-3 w-100">
+            <button id="log-btn" type="button" class="btn btn-danger rounded-pill w-90">過去の運勢を確認する</button>
         </div>
     `
     container.querySelectorAll("#back-btn")[0].addEventListener("click", function(){
